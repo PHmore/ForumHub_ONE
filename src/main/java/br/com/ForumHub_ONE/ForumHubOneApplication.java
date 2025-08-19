32f49076c93bcbd2e202e3174bfbbdb6fdd66730
PHmore <patryckhenryck401@gmail.com>
2025-08-19 09:59:17 -0300

    Initial commit

---
+++ src/main/java/br/com/ForumHub_ONE/ForumHubOneApplication.java
@@ -0,0 +1,13 @@
+package br.com.ForumHub_ONE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumHubOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumHubOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ForumHub_ONE/ForumHubOneApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ForumHub_ONE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumHubOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
